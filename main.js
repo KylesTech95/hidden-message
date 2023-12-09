a5575af8b0d526aaf829e0fb94d67ed1edbd10c4
kylestech95 <kylestech95@outlook.com>
2023-12-08 20:30:06 -0500

    committed

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let logs_arr = document.querySelectorAll('.log')
 let body = document.body;
 let logList = document.querySelector('.log-list')
 let yPos
-let i = 3;
+let i = 0;
 let array = []
 let message2 = document.querySelector('.message-container2')
 let message = document.querySelector('.hello-world')
@@ -11,13 +11,15 @@ function randomWord(){
     let nextWord = words[Math.floor(Math.random()*words.length)]
     return nextWord
 }
+let log_top = document.querySelectorAll('.log-top')
+let log_bottom = document.querySelectorAll('.log-bottom')
+
 const words = ['Fork for Fun','My name is Kyle','Hello World',`${new Date().getMonth()}-${new Date().getDate()}-${new Date().getFullYear()}`,'You are beautiful']
 window.addEventListener('mousemove',e=>{
     mouse={x:e.pageX,y:e.pageY}
     array.push(mouse.y)
     yPos = array.pop()
 let right = (mouse.x > (logList.getBoundingClientRect().x + logList.getBoundingClientRect().width))
-
 let left = (mouse.x < logList.getBoundingClientRect().x)
 
     if(right&&right+5 || left&&left-5){
@@ -29,25 +31,26 @@ logs_arr.forEach((log,i)=>{
     let children = [...log.children],
     top = children[0],
     bottom = children[1]
+    tHeight = top.getBoundingClientRect().height
+    bHeight = bottom.getBoundingClientRect().height
     log.addEventListener('mouseenter',e=>{
         e.preventDefault()
-        top.style = `transform:translate(0,${(yPos-top.getBoundingClientRect().height)-25}px);background:firebrick;transition:.2s;`
-        bottom.style = `transform:translate(0,${(yPos - top.getBoundingClientRect().height)+25}px);background:firebrick;transition:.2s;`
+        i++
+        //insert into log
+        
+        top.style=`height:${yPos < logList.clientHeight/2 ? yPos - 75 : yPos - 75}px;transition:.15s;`
+        bottom.style=`height:${yPos > logList.clientHeight/2 ? (body.clientHeight - yPos) - 75 : (body.clientHeight - yPos) - 25}px;transition:.15s;`
         setTimeout(()=>{
-            top.style = `height:50%;transition:.15s;`
-            bottom.style = `height:50%;transition:.15s;`
-        },25 * i)
-    })
-    log.addEventListener('mouseleave',e=>{
-        e.preventDefault()
-        i+=.15
-        top.style = `transform:translate(0,${(yPos-top.getBoundingClientRect().height)-100}px);transition:1s;background:brown`
-        bottom.style = `transform:translate(0,${(yPos - top.getBoundingClientRect().height)+100}px);transition:1s;background:brown;`
+        top.style=`height:${top.clientHeight+75}px;transition:.15s;`
+        bottom.style=`height:${bottom.clientHeight+75}px;transition:.15s;`
+        },25 * (i+1))
         setTimeout(()=>{
-            top.style = `height:50%;transition:.15s;`
-            bottom.style = `height:50%;transition:.15s;`
-        },25 * i)
+            top.style=`height:50%;transition:.15;`;
+            bottom.style=`height:50%;transition:.15;`
+        },50 * (i+1))
+
     })
+
 })
 logList.addEventListener('mouseover',e=>{
     message.style = 'display:block'
@@ -67,3 +70,14 @@ logList.addEventListener('mouseout',e=>{
     }
         
 })
+function B_ON(){
+log_top.forEach(top=>top.classList.add('border-active'))
+log_bottom.forEach(bottom=>bottom.classList.add('border-active'))
+
+}
+
+function B_OFF(){
+    log_top.forEach(top=>top.classList.remove('border-active'))
+log_bottom.forEach(bottom=>bottom.classList.remove('border-active'))
+
+}
